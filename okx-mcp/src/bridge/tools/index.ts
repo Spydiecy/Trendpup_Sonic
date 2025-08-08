f206bce2e14c9a842dd0176a462cf82fc88a3b17
bekfastbek <kinshukkataria4@gmail.com>
2025-08-08 02:20:03 +0000

    added the bridge operations

---
+++ okx-mcp/src/bridge/tools/index.ts
@@ -0,0 +1,5 @@
+export * from "./buildCrossChainSwap";
+export * from "./getBridgeTokenPairs";
+export * from "./getCrossChainQuote";
+export * from "./getSupportedBridges";
+export * from "./getSupportedTokens";
